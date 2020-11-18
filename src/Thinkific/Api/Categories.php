c474ae6fdd8960cd013c93bead636f9e9c752969
Bo Gunnarson <bg@golearn.dk>
2020-11-18 12:50:09 +0100

    Added Categories service

---
+++ src/Thinkific/Api/Categories.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of the Thinkific library.
+ *
+ * (c) Graphem Solutions <info@graphem.ca>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Thinkific\Api;
+
+class Categories extends AbstractApi{
+  
+    /**
+     * @var string
+     */
+    protected $service = 'collections';
+}
